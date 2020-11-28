f8816b26cb5689ee111c8e5101808378d7f2d8d6
sunxili520 <807071983@qq.com>
2020-11-29 04:09:51 +0800

    feign

--- rms-index/src/main/java/com/kyoxue/RmsIndexApplication.java
+++ rms-index/src/main/java/com/kyoxue/RmsIndexApplication.java
@@ -3,6 +3,7 @@ package com.kyoxue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author admin
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class RmsIndexApplication {
     public static void main(String[] args) {
         SpringApplication.run(RmsIndexApplication.class,args);
--- rms-index/src/main/java/com/kyoxue/controller/IndexController.java
+++ rms-index/src/main/java/com/kyoxue/controller/IndexController.java
@@ -1,11 +1,15 @@
 package com.kyoxue.controller;
 
+import com.kyoxue.api.IProductApi;
+import com.kyoxue.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author admin
@@ -17,13 +21,21 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("index")
 public class IndexController {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    /*@Autowired
+    private RestTemplate restTemplate;*/
 
-    private final static String PRODUCT_URL = "http://RMS-PRODUCT/product/list";
+    @Autowired
+    private IProductApi productApi;
+//    private final static String PRODUCT_URL = "http://RMS-PRODUCT/product/list";
     @RequestMapping("show")
     @ResponseBody
-    public String show(){
-        return restTemplate.getForObject(PRODUCT_URL, String.class);
+    public List<Product> show(){
+        try {
+            return productApi.getProducts();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+//        return restTemplate.getForObject(PRODUCT_URL, String.class);
     }
 }
